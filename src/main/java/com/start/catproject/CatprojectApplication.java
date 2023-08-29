f4e5b66de56b7d2564adecdffbab18859a6cfad1
sussa3007 <sussa1933@gmail.com>
2023-08-29 20:34:37 +0900

    Init Server

---
+++ src/main/java/com/start/catproject/CatprojectApplication.java
@@ -0,0 +1,13 @@
+package com.start.catproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatprojectApplication.class, args);
+	}
+
+}
